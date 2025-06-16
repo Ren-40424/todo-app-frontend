f326932b4dc8823283fcbc57ca1b1e81602ec75b
Ren-40424 <takashimaren0527@gmail.com>
2025-06-16 01:34:50 +0000

    ALB用にヘルスチェック対応

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TodoList from '@/components/TodoList.vue'
 import Callback from '@/components/Callback.vue'
+import Healthcheck from '../components/Healthcheck.vue'
 
 const routes = [
     { path: '/', component: TodoList },
-    { path: '/callback', component: Callback }
+    { path: '/callback', component: Callback },
+    { path: '/healthcheck', component: Healthcheck },
 ]
 
 export const router = createRouter({
